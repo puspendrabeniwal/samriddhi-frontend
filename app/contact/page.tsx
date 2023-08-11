09575237de4ddcd319f96e5478a7b0f0c2bfb721
pooja_shami <poojashami@myhaulstore.com>
2023-08-10 16:39:32 -1200

    remove Map and Banner Page and add Address in Contact Page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -37,50 +37,8 @@ export default function Contact() {
     <>
       <Toast ref={toast} />
       <main>
-        <div className="productBannerTop">
-          <h1>Contact Us</h1>
-        </div>
-
-        <section className="defaultPaddingTB">
-          <div className="container">
-            <div className="col-xl-10 col-lg-11 mx-auto">
-              <div className="row">
-                <div className="col-lg-4 text-center mb-4 mb-lg-0">
-                  <img src="image/PhoneNumber.png" alt="" />
-                  <h4 className="mt-lg-3 KumbhSans-Bold">Phone Number</h4>
-                  <h6 className="mt-2 KumbhSans-Light">+91 1234567890</h6>
-                  <h6 className="mt-2 KumbhSans-Light">+91 1234567890</h6>
-                </div>
-
-                <div className="col-lg-4 text-center mb-4 mb-lg-0">
-                  <img src="image/PhoneNumber.png" alt="" />
-                  <h4 className="mt-lg-3 KumbhSans-Bold">Address</h4>
-                  <h6 className="mt-2 KumbhSans-Light">
-                    176 W street name, Area bane, State name 1001425
-                  </h6>
-                </div>
-
-                <div className="col-lg-4 text-center">
-                  <img src="image/PhoneNumber.png" alt="" />
-                  <h4 className="mt-lg-3 KumbhSans-Bold">Email Address</h4>
-                  <h6 className="mt-2 KumbhSans-Light">contact@frames.com</h6>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
         <section className="p-3 p-lg-0">
           <div className="row mx-lg-0">
-            <div className="col-lg-6 px-0 px-lg-0 mb-lg-0">
-              <div className="map-responsive">
-                <iframe
-                  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyA0s1a7phLN0iaD6-UE7m4qP-z21pH0eSc&q=Eiffel+Tower+Paris+France"
-                  className="googleMap"
-                ></iframe>
-              </div>
-            </div>
-
             <div className="col-lg-6 p-lg-5 py-5 contactUsSection">
               <h6 className="KumbhSans-Regular mb-3">CONTACT US</h6>
               <h1 className="">
@@ -261,6 +219,40 @@ export default function Contact() {
                   </form>
                 )}
               </Formik>
+            </div>
+            <div className="col-lg-6 px-0 px-lg-0 mb-lg-0 contactUsSection">
+              <div className="map-responsive">
+                {" "}
+                <div className="col-lg-5 p-lg-5">
+                  <div className="mb-5 d-flex align-items-start contactAddress">
+                    <img
+                      src="image/locationpin.png"
+                      className=""
+                      alt="product image"
+                    />
+                    <div className="ms-4">
+                      <h5>Address</h5>
+                      <h6>
+                        1B-Block, BHIVE Workspace - No.112, AKR Tech Park, "A"
+                        and, 7th Mile Hosur Rd, <br />
+                        Krishna Reddy Industrial Area, Bengaluru, Karnataka
+                        560068
+                      </h6>
+                    </div>
+                  </div>
+                  <div className="mb-5 d-flex align-items-start contactAddress">
+                    <img
+                      src="image/Mailicon.png"
+                      className=""
+                      alt="product image"
+                    />
+                    <div className="ms-4">
+                      <h5>Email Address</h5>
+                      <h6>support@samriddhiframes.com</h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
